97612a36a4fdded3b64051f212c934f88c928263
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2024-12-11 03:08:00 -0800

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,43 +18,43 @@ function seasonalMode() {
 var currentMonth = new Date().getMonth();
 
     // Test other months
-    var currentMonth = currentMonth - 11;
-    console.log(currentMonth);
+    // var currentMonth = currentMonth - 11;
+    // console.log(currentMonth);
 
     if (currentMonth === 11) {
         // December
         document.getElementById("bg-picker").classList.add('christmas-theme');
-        document.querySelector("button.seasonal-button span").innerText = 'Christmas Seasonal Theme';
+        document.querySelector("button.seasonal-button span").innerText = 'Christmas Theme';
         document.querySelector("button.seasonal-button i").classList.add('fa-gift');
         
         } else if (currentMonth === 0) {
         // January
         document.getElementById("bg-picker").classList.add('ny-theme');
-        document.querySelector("button.seasonal-button span").innerText = "New Year's Seasonal Theme";
+        document.querySelector("button.seasonal-button span").innerText = "New Year's Theme";
         document.querySelector("button.seasonal-button i").classList.add('fa-champagne-glasses');
 
         } else if (currentMonth >= 2 && currentMonth <= 4) {
         // Spring months March - May
         document.getElementById("bg-picker").classList.add('spring-theme');
-        document.querySelector("button.seasonal-button span").innerText = 'Spring Seasonal Theme';
+        document.querySelector("button.seasonal-button span").innerText = 'Spring Theme';
         document.querySelector("button.seasonal-button i").classList.add('fa-seedling');
 
         } else if (currentMonth >= 5 && currentMonth <= 7) {
         // Summer months June - August
         document.getElementById("bg-picker").classList.add('summer-theme');
-        document.querySelector("button.seasonal-button span").innerText = 'Summer Seasonal Theme';
+        document.querySelector("button.seasonal-button span").innerText = 'Summer Theme';
         document.querySelector("button.seasonal-button i").classList.add('fa-umbrella-beach');
 
         } else if (currentMonth >= 8 && currentMonth <= 10) {
         // Autumn months September - November
         document.getElementById("bg-picker").classList.add('autumn-theme');
-        document.querySelector("button.seasonal-button span").innerText = 'Autumn Seasonal Theme';
+        document.querySelector("button.seasonal-button span").innerText = 'Autumn Theme';
         document.querySelector("button.seasonal-button i").classList.add('fa-wind');
 
         } else {
         // Winter/remaining months December - February 
         document.getElementById("bg-picker").classList.add('winter-theme');
-        document.querySelector("button.seasonal-button span").innerText = 'Winter Seasonal Theme';
+        document.querySelector("button.seasonal-button span").innerText = 'Winter Theme';
         document.querySelector("button.seasonal-button i").classList.add('fa-snowflake');
 
     }
