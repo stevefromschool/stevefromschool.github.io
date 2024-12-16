b16acdb2914c58c18f11862d6e46fe7814ebdb07
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2024-12-16 03:17:42 -0800

    Update loops.js

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -1,6 +1,6 @@
 
 // Only start after the whole DOM has loaded
-window.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
 
 // Loop through all <li> tags and count 
     var favItems = document.querySelectorAll("ul#favsList li");
