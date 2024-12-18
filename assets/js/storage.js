023182c082ee8e5724bb67ff02a6a22a316facac
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2024-12-17 22:02:36 -0800

    update

--- assets/js/storage.js
+++
@@ -1,8 +0,0 @@
-// Save email address and show it back as an alert
-function saveData() {
-    var fetchedData = document.getElementById('email').value;
-    localStorage.setItem('savedData', fetchedData);
-
-    var archivedData = localStorage.getItem('savedData');
-    alert("Hello " + "'" + archivedData + "'" + ". Thank you for subscribing to my totally hypothetical fake newsletter!");
-}
