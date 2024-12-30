bebc84c794da9a2722a9a0296c8958b3c12f8414
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2024-12-29 20:34:19 -0800

    Update loops.js

--- assets/js/loops.js
+++ assets/js/loops.js
@@ -8,9 +8,11 @@ window.addEventListener("load", function() {
     const favsNumber = favItems.length
 
     // Count up to the favsNumber, put it back in #favsTotal. Only start counting after it is in view (scroll spy). 
-      new CountUp('favsTotal', favsNumber, {
-        enableScrollSpy: true,        
-        scrollSpyOnce: true,
-      });
+      new CountUp('favsTotal', favsNumber);
+        if (!countUp.error) {
+            countUp.start();
+        } else {
+            console.error(countUp.error);
+        }
 
 });
