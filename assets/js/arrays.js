d90538e9ac87d79ae92044330ff77b62f318d088
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2025-01-02 20:36:30 -0800

    playing with async

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -1,20 +1,19 @@
 // Loop through the JSON array in data.json and output it on the page. 
 
 const xmlhttp = new XMLHttpRequest();
+
+xmlhttp.open("GET", "assets/js/data.json", false);
+
 xmlhttp.onload = function() {
-const myObj = JSON.parse(this.responseText);
+    const myObj = JSON.parse(this.responseText);
+    let favouriteList = "";
 
-let favouriteList = "";
+    for(let i = 0, l = myObj.data.length; i < l; i++) {
 
-for(let i = 0, l = myObj.data.length; i < l; i++) {
+        favouriteList += "<li>" + myObj.data[i].favourite + "</li>"; 
 
-favouriteList += "<li>" + myObj.data[i].favourite + "</li>"; 
-
-document.getElementById("favsList").innerHTML = favouriteList;
-
+        document.getElementById("favsList").innerHTML = favouriteList;
+    }
 }
 
-}
-xmlhttp.open("GET", "assets/js/data.json");
 xmlhttp.send();
-
