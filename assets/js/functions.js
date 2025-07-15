2d4ae25c337908fb26fa18ba77e9aa5d5fddc5c7
Steve Quilala <7945298+stevefromschool@users.noreply.github.com>
2025-07-14 22:02:42 -0700

    Update Theming options

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,12 +4,19 @@ function darkMode() {
     var darkPicker = document.querySelector("#bg-picker");
     darkPicker.classList.remove("seasonal-bg");
     darkPicker.classList.remove("default-bg");
-    darkPicker.classList.toggle("dark-bg");
+    darkPicker.classList.add("dark-bg");
 }
 
 function seasonalMode() {
     var seasonalPicker = document.querySelector("#bg-picker");
     seasonalPicker.classList.remove("dark-bg");
     seasonalPicker.classList.remove("default-bg");
-    seasonalPicker.classList.toggle("seasonal-bg");
+    seasonalPicker.classList.add("seasonal-bg");
+}
+
+function defaultMode() {
+    var defaultPicker = document.querySelector("#bg-picker");
+    defaultPicker.classList.remove("dark-bg");
+    defaultPicker.classList.remove("seasonal-bg");
+    defaultPicker.classList.add("default-bg");
 }
